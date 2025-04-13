0d78d25b3678f642a8b70ce18677096f12ff8b9d
Lara Fernandes <lara.fernandes@temple.edu>
2025-04-13 19:16:52 -0400

    Adjusted paths for settings access

--- image_processing_app/image_processing_app/wsgi.py
+++ image_processing_app/image_processing_app/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_processing_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_processing_app.image_processing_app.settings')
 
 application = get_wsgi_application()
